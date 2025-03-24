1c25a14ddd88eaad4e6eba6509804ecd299b8650
andiselimi <153734914+andiselimi@users.noreply.github.com>
2025-03-25 00:47:36 +0100

    add raw dashboard for testing

---
+++ demo-components/vue/CustomDividerDemo.vue
@@ -0,0 +1,51 @@
+<template>
+  <Section
+    contentStyle="align-items: flex-start;"
+  >
+    <div
+      class="turtle-panes__section-description"
+    >
+      <h1 class="turtle-panes__heading">Custom divider ?</h1>
+      <p class="turtle-panes__description">We got you covered.</p>
+      <div class="demo-intro__source-code">
+          <a :href="examplesUrl.react"><ReactIcon /> </a>
+          <a :href="examplesUrl.vue"><VueIcon /> </a>
+      </div>
+    </div>
+    <TurtlePanes
+      style="
+        margin: 20px 0px;
+        border-radius: 20px;
+        background-color: var(--white);
+      "
+    >
+      <TurtlePane>
+        <div style="width: 30cqw" class="turtle-panes__demo-box"></div>
+        <template v-slot:divider>
+          <div class="turtle-panes__custom-divider">🐢</div>
+        </template>
+      </TurtlePane>
+      <TurtlePane>
+        <div style="width: 30cqw" class="turtle-panes__demo-box"></div>
+        <template v-slot:divider>
+          <div class="turtle-panes__custom-divider">🏎️</div>
+        </template>
+      </TurtlePane>
+      <TurtlePane>
+        <div style="width: 30cqw" class="turtle-panes__demo-box"></div>
+      </TurtlePane>
+    </TurtlePanes>
+  </Section>
+</template>
+<script setup lang="ts">
+import { TurtlePanes, TurtlePane } from "@turtle-panes/vue";
+import "@turtle-panes/vue/style";
+import Section from "./Section.vue";
+import { ReactIcon, VueDotjsIcon as VueIcon } from "vue3-simple-icons";
+const examplesUrl = {
+  react: `${import.meta.env.VITE_GITHUB_EXAMPLES_REPO}/react/CustomDividerDemo.tsx`,
+  vue: `${import.meta.env.VITE_GITHUB_EXAMPLES_REPO}/vue/CustomDividerDemo.vue`,
+}
+</script>
+<style lang="scss" src="../styles/CustomDividerDemo.scss">
+</style>
---
+++ demo-components/vue/FrameworkSupport.vue
@@ -0,0 +1,29 @@
+<template>
+  <Section
+    contentStyle="align-items: center;"
+  >
+    <div
+      class="turtle-panes__section-description"
+    >
+      <h1 class="turtle-panes__heading">Framework support</h1>
+      <p class="turtle-panes__description">
+        Turtle-Panes plays nicely with some of the major frameworks
+      </p>
+    </div>
+    <div class="demo-framework-support__icons">
+      <a class="demo-intro__button" href="/vue/getting-started">
+        <VueDotjsIcon />
+        <span>Vue Docs</span>
+      </a>
+      <a class="demo-intro__button" href="/react/getting-started">
+        <ReactIcon />
+        <span>React Docs</span>
+      </a>
+    </div>
+  </Section>
+</template>
+<script setup lang="ts">
+import { ReactIcon, VueDotjsIcon } from "vue3-simple-icons";
+import Section from "./Section.vue";
+</script>
+<style lang="scss" src="../styles/FrameworkSupport.scss"></style>
